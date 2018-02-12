338e050b9c915a825c48308b120a616bdc3cdeb3
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2018-02-12 21:04:39 +0000

    feat(mvc/api): Introducing Products Api Controller
    Added ProductsController.
    Used IActionResult to return respone with appropriate error codes in combination with Ok and BadRequest.
    Changed application url port to 8888 so that it is easier to remember.
    Tested with PostMan.

---
+++ DutchTreat/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using DutchTreat.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace DutchTreat.Controllers
+{
+  /// <summary>
+  /// webapi used to be a separate library to expose api calls. 
+  /// in .net core it is all in one library. 
+  /// </summary>
+  [Route("api/[Controller]")]
+  public class ProductsController : Controller
+  {
+    private readonly IDutchRepository _dutchRepository;
+    private readonly ILogger<ProductsController> _logger;
+
+    public ProductsController(IDutchRepository dutchRepository, ILogger<ProductsController> logger)
+    {
+      _dutchRepository = dutchRepository;
+      _logger = logger;
+    }
+
+    /// <summary>
+    /// How do we handle exceptions ?
+    /// What do I return to the users?
+    /// Setting return type to IActionResult
+    /// simplifies HttpResponse codes. And Error Handling. 
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public IActionResult Get()
+    {
+      try
+      {
+        return Ok(_dutchRepository.GetAllProducts());
+      }
+      catch (Exception ex)
+      {
+        _logger.LogError($"Failed to get products:{ex}");
+        return BadRequest("Failed to get products");
+      }
+    }
+  }
+}
