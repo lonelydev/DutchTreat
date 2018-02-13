8af384614710ccab02b6cf85e362d5d017c3c28b
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2018-02-13 00:54:22 +0000

    feat(mvc/automapper): Introducing Automapper
    Automapper - package to map model to viewmodel in an elegant way.
    Requires extension package for support with .net core dependency injection.
    
    Mappings are added in a profile class in fluent syntax.
    Same name mappings are dealt with seamlessly.
    Where attribute/field names differ, mappings have to be specified explicitly.
    Reverse mappings can be specified too using method of the same name.
    Make use of this in get, getbyid and post of orders.

---
+++ DutchTreat/Data/DutchMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using DutchTreat.Data.Entities;
+using DutchTreat.ViewModels;
+
+namespace DutchTreat.Data
+{
+  /// <summary>
+  /// Profile is a container around any mappings you want to setup. 
+  /// 
+  /// </summary>
+  public class DutchMappingProfile : Profile
+  {
+    public DutchMappingProfile()
+    {
+      CreateMap<Order, OrderViewModel>()
+        .ForMember(ov => ov.OrderId,
+        map => map.MapFrom(o => o.Id))
+        .ReverseMap();
+    }
+  }
+}
