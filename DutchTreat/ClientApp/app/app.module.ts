61863bdcd79c7e9fb5af3f1d83e177302347279a
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2018-02-18 15:19:36 +0000

    feat(typescript): Angular cli
    npm install -f @angular/cli
    ng new dutch-app --minimal
    ng build
    ng serve

---
+++ DutchTreat/ClientApp/app/app.module.ts
@@ -0,0 +1,24 @@
+// a different way of loading modules from what we saw in the 
+// typescript intro. 
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
+// essential file, this is the bootstrapping file. 
+// there isn't really any code here apart from what's below
+// @xyz is called a decorator. 
+// tells what we need for the app
+// 
+@NgModule({
+  declarations: [
+    AppComponent // the thing we are going to use on a page
+  ],
+  imports: [
+    BrowserModule  //how to host it on a browser page?! wtf
+  ],
+  providers: [],
+  bootstrap: [AppComponent] //what do i load first. this might load other components
+})
+export class AppModule { }
