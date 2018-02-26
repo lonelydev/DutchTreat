5b1f6f5240343b47c56573567c1aa00b0ff07ffa
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2018-02-26 21:42:35 +0000

    feat(envTagHelper): Environment Tag helpers
    using `<environment names="env1,env2">` in `cshtml`.
    `asp-append-version` tag helper
    `asp-fallback-src` taghelper
    `asp-fallback-test` taghelper

---
+++ DutchTreat/gulpfile.js
@@ -0,0 +1,21 @@
+﻿/// <binding AfterBuild='default' />
+/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('minify', function () {
+  // js files in js directory and all subdirectories
+  return gulp.src('wwwroot/js/**/*.js')
+    //fluent syntax - minify each file
+    .pipe(uglify())
+    // concatenate all minified files into one bundle
+    .pipe(concat('dutchtreat.min.js'))
+    .pipe(gulp.dest('wwwroot/dist'));
+});
+
+gulp.task('default', ["minify"]);
